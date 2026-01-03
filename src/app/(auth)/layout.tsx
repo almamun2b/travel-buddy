2dfdcb6315f4bf2a4d116e454b83a36d2986f323
almamun2b <almamun2b@gmail.com>
2026-01-03 23:09:59 +0600

    user

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,4 @@
-import Footer from "@/components/shared/Footer";
-import Navbar from "@/components/shared/navbar/Navbar";
 import ErrorBoundary from "@/components/shared/ErrorBoundary";
-import { Suspense } from "react";
 
 export default function AuthLayout({
   children,
@@ -9,20 +6,8 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <ErrorBoundary>
-        <Suspense fallback={<div className="h-14 w-full bg-background border-b" />}>
-          <Navbar />
-        </Suspense>
-      </ErrorBoundary>
-      <main className="min-h-dvh container mx-auto px-6">
-        <ErrorBoundary>
-          {children}
-        </ErrorBoundary>
-      </main>
-      <ErrorBoundary>
-        <Footer />
-      </ErrorBoundary>
-    </>
+    <main className="min-h-dvh container mx-auto px-6">
+      <ErrorBoundary>{children}</ErrorBoundary>
+    </main>
   );
 }
