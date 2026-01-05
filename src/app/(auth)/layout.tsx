1051688677d00c744beb1e4742cff7f6b3657e20
almamun2b <almamun2b@gmail.com>
2026-01-05 22:06:41 +0600

    proxy.ts

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,15 @@
 import ErrorBoundary from "@/components/shared/ErrorBoundary";
-
-export default function AuthLayout({
+import { me } from "@/services/auth/me";
+import { redirect } from "next/navigation";
+export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = await me();
+  if (user?.success && user.data) {
+    redirect("/");
+  }
   return (
     <main className="min-h-dvh container mx-auto px-6">
       <ErrorBoundary>{children}</ErrorBoundary>
