d727a58a6ac10d5f40d1acafaa4703b21bea2a74
almamun2b <almamun2b@gmail.com>
2026-01-10 15:55:09 +0600

    fix message

--- src/services/payment/cancelSubscription.ts
+++ src/services/payment/cancelSubscription.ts
@@ -1,35 +1,34 @@
 "use server";
 
 import { $fetch } from "@/lib/fetch";
+import { revalidateTag } from "next/cache";
 
 export interface CancelSubscriptionResponse {
   success: boolean;
   message: string;
 }
 
-export const cancelSubscription =
-  async (): Promise<CancelSubscriptionResponse> => {
-    try {
-      const result = await $fetch.post<CancelSubscriptionResponse>(
-        "/payment/subscription/cancel"
-      );
-      return (
-        result || {
-          success: false,
-          message: "Failed to cancel subscription. Please try again.",
-        }
-      );
-    } catch (error: unknown) {
-      console.log("CANCEL_SUBSCRIPTION_ERROR:", error);
-
-      return {
-        success: false,
-        message:
-          process.env.NODE_ENV === "development"
-            ? error instanceof Error
-              ? error.message
-              : "Unknown error"
-            : "Subscription cancellation failed. Please try again.",
-      };
+export const cancelSubscription = async () => {
+  try {
+    const result = await $fetch.post<CancelSubscriptionResponse>(
+      "/payment/subscription/cancel"
+    );
+    if (result?.success) {
+      revalidateTag("subscription-plans", "");
+      revalidateTag("subscription-status", "");
     }
-  };
+    return result;
+  } catch (error: unknown) {
+    console.log("CANCEL_SUBSCRIPTION_ERROR:", error);
+
+    return {
+      success: false,
+      message:
+        process.env.NODE_ENV === "development"
+          ? error instanceof Error
+            ? error.message
+            : "Unknown error"
+          : "Subscription cancellation failed. Please try again.",
+    };
+  }
+};
